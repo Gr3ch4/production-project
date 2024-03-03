6462407676cd3218781c363fa590740dd1c893c1
Gr3ch4 <grisha.salmin.2002@mail.ru>
2024-03-03 17:06:03 +0300

    add modal, add portalm change navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,13 +10,26 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+    const [isAuthModal, setIsAuthModal] = useState(false);
     const { t } = useTranslation();
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev)
+    }, [])
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button 
+                theme={ButtonTheme.CLEAR_INVERTED} 
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                 {/* eslint-disable-next-line max-len */}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi cupiditate quidem exercitationem quia error tempora inventore. Nam velit illo provident culpa impedit minus ullam atque magnam asperiores quam. Asperiores, recusandae.
+            </Modal>
         </div>
     );
 };
